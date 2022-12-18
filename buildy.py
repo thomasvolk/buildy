725d7700be152b493d1e9ccf4b52534e0c03e050
Thomas Volk (MacBook) <info@thomasvolk.de>
2022-12-18 13:35:54 +0100

    init

---
+++ buildy.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from subprocess import Popen
+import json
+from dataclasses import dataclass, asdict
+
+builds = dict()
+
+@dataclass
+class Repository:
+    url: str
+    branch: str
+    tag: str
+
+class Build:
+    def __init__(self, repo):
+        self.repo = repo 
+        self.__process = Popen('echo "start process $$"; sleep 20; echo "end process $$"', shell=True)
+
+    @property
+    def running(self):
+        return self.__process.poll() == None
+
+    @property
+    def id(self):
+        return str(self.__process.pid)
+
+    @property
+    def json(self):
+        return json.dumps(
+            {
+                "pid": self.id,
+                "running": self.running,
+                "repository": asdict(self.repo)
+            }
+        )
+
+class BuildyServer(BaseHTTPRequestHandler):
+    def __split_path(self):
+        return self.path[1:].split('/')
+
+    def do_POST(self):
+        path = self.__split_path()
+        if(path == ['build']):
+            content_length = int(self.headers['Content-Length'])
+            build_setup = json.loads(self.rfile.read(content_length).decode('utf-8'))
+            self._start_build(build_setup)
+        else:
+            self.send_response(404)
+
+    def _start_build(self, build_setup):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        build = Build(Repository("", "", "")) 
+        builds[build.id] = build
+        self.wfile.write(bytes(json.dumps({"id": build.id}), "utf-8"))
+
+    def do_GET(self):
+        path = self.__split_path()
+        if(path == ['']):
+            pass
+        elif(path[0] == 'build'):
+            self._get_build(path[1])
+        else:
+            self.send_response(404)
+
+    def _get_build(self, id):
+        build = builds.get(id)
+        if(build == None):
+            self.send_response(404)
+            return
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(bytes(build.json, "utf-8"))
+
+
+hostName = "localhost"
+serverPort = 8080
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), BuildyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
