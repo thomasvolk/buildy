cfc174fa75851159566fecde0629d2fbc0acf5d1
Thomas Volk (MacBook) <info@thomasvolk.de>
2022-12-22 20:14:36 +0100

    rename ctl script

---
+++ buildy-ctl.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from subprocess import Popen
+import os
+
+FILE='./buildy.py'
+
+process = Popen(FILE, shell=True)
+
+current = os.path.getmtime(FILE)
+
+while True:
+  mtime = os.path.getmtime(FILE)
+  if mtime != current:
+    current = mtime
+    process.kill()
+    process.wait()
+    process = Popen(FILE, shell=True)
+
+  
