f857434f6d3bd210ccadbfc7773d08675211f7c0
Thomas Volk (MacBook) <info@thomasvolk.de>
2022-12-22 20:54:34 +0100

    tests first

--- buildy-test.py
+++ buildy-test.py
@@ -8,15 +8,20 @@ BUILDY_URL='http://localhost:9000'
 def mk_repo(name, sleep_interval):
     return f"""mkdir -p {os.getcwd()}/temp/repos/{name} &&
 cd {os.getcwd()}/temp/repos/{name} &&
-echo "all:\n\tsleep {sleep_interval}" > Makefile &&
+echo "all:\n\tsleep {sleep_interval}"\n\ttouch build-complete.txt > Makefile &&
 git init && git add --all && git commit -m"{name}" """
 
-prepare_tests = f"rm -rf {os.getcwd()}/temp && " + mk_repo(10, 0.01) + " && " + mk_repo(100, 0.1) + " && " + mk_repo(1000, 1)
+prepare_tests = f"""rm -rf {os.getcwd()}/temp &&
+mkdir -p {os.getcwd()}/temp/buildy &&
+{mk_repo(10, 0.01)} &&
+{mk_repo(100, 0.1)} &&
+{mk_repo(1000, 1)}"""
 
 process = Popen(prepare_tests, shell=True)
 assert 0 == process.wait()
 
 print("test data created")
+buildy_dir= f"{os.getcwd()}/temp/buildy"
 
 response = requests.post(f"{BUILDY_URL}/build", json={"url": f"{os.getcwd()}/temp/repos/1000"})
 assert 201 == response.status_code
@@ -26,3 +31,5 @@ response = requests.get(f"{BUILDY_URL}/build/{build_id}")
 assert 200 == response.status_code
 build = response.json()
 print(build)
+assert os.path.exists(f"{buildy_dir}/{build_id}")
+assert os.path.exists(f"{buildy_dir}/{build_id}/build-complete.txt")
